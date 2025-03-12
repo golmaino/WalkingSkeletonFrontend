70fb943857cbea901c360cb74c206443c75905eb
golmaino <golmaino@gmx.at>
2025-03-12 23:21:36 +0100

    feat: implement moving-request with updated playwright

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { MoveRequestComponent } from './moving-request/moving-request.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'moving', component: MoveRequestComponent }
 ];
-
